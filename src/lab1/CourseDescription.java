07a38ad0511b39aa9dc6c5b53f83da2c8ca88ea0
mking@my.wctc.edu <>
2013-01-30 21:00:10 -0600

    Added Abstract superclass & properties

---
+++ src/lab1/CourseDescription.java
@@ -0,0 +1,15 @@
+package lab1;
+
+/**
+ * Describe responsibilities here.
+ *
+ * @author      Mary King
+ * @version     1.00
+ */
+public abstract class CourseDescription {
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+    private boolean hasPrerequisite;
+
+}
